7c30a44b96c43e90ceac051a8d2067d4231d2802
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2025-02-08 19:31:32 +0600

    feat(styles): add Google fonts and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      primary: "Playfair Display",
+      secondary: "Mulish",
+    },
     extend: {},
   },
   plugins: [],
