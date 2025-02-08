07be9e4d7e2e71726841752430855be17cfc0b21
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2025-02-08 20:06:14 +0600

    feat(styles): update global styles and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
       primary: "Playfair Display",
       secondary: "Mulish",
     },
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1192px",
+    },
+    extend: {
+      colors: {
+        primary: "#0E1112",
+        grey: "#484B4B",
+        accent: "#EEF7F9",
+      },
+    },
   },
   plugins: [],
 };
