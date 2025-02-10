0bb2d9ac5330559ce7921c6498c243cfb1ee011c
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2025-02-10 13:39:04 +0600

    feat(portfolio): add portfolio section with responsive design
    chore(about): minor import naming correction

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import womanImage from "../assets/img/about/woman.png";
+import WomanImage from "../assets/img/about/woman.png";
 import { Link } from "react-router-dom";
 
 const About = () => {
@@ -8,7 +8,7 @@ const About = () => {
       <div className="container relative h-full">
         <div className="flex h-full flex-col items-center justify-center gap-x-24 text-center lg:flex-row lg:pt-16 lg:text-left">
           <div className="order-2 max-h-96 flex-1 overflow-hidden lg:order-none lg:max-h-max">
-            <img src={womanImage} alt="Woman Image" />
+            <img src={WomanImage} alt="Woman Image" />
           </div>
           <div className="z-10 flex flex-1 flex-col items-center justify-center pb-14 pt-32 lg:w-auto lg:items-start lg:pt-0">
             <h1 className="h1">about me</h1>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import Image1 from "../assets/img/portfolio/1.png";
+import Image2 from "../assets/img/portfolio/2.png";
+import Image3 from "../assets/img/portfolio/3.png";
+import Image4 from "../assets/img/portfolio/4.png";
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
-  return <section className="section bg-orange-200">Portfolio</section>;
+  return (
+    <section className="section">
+      <div className="container relative h-full">
+        <div className="flex h-full flex-col items-center justify-start gap-x-24 pb-8 pt-24 text-center lg:flex-row lg:pt-36 lg:text-start">
+          <div className="flex flex-col lg:items-start">
+            <h1 className="h1">portfolio</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+              <b>Dolore recusandae</b> aliquid vel ea vitae dolor eius commodi
+              ipsum dolorum.
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+              quibusdam rerum et natus?
+            </p>
+            <Link
+              to={"/contact"}
+              className="btn mx-auto mb-[30px] w-fit lg:mx-0"
+            >
+              Hire me
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 lg:gap-2">
+            <div className="h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[300px]">
+              <img
+                className="h-full object-cover transition-all duration-500 hover:scale-[110%] lg:h-[220px]"
+                src={Image1}
+                alt=""
+              />
+            </div>
+            <div className="h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[300px]">
+              <img
+                className="h-full object-cover transition-all duration-500 hover:scale-[110%] lg:h-[220px]"
+                src={Image2}
+                alt=""
+              />
+            </div>
+            <div className="h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[300px]">
+              <img
+                className="h-full object-cover transition-all duration-500 hover:scale-[110%] lg:h-[220px]"
+                src={Image3}
+                alt=""
+              />
+            </div>
+            <div className="h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[300px]">
+              <img
+                className="h-full object-cover transition-all duration-500 hover:scale-[110%] lg:h-[220px]"
+                src={Image4}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
