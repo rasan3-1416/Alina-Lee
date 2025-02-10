f06013c1e074dfc691d15b37d0fa4b16a31984e6
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2025-02-10 11:22:48 +0600

    feat(about): add about section with responsive design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import womanImage from "../assets/img/about/woman.png";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return <section className="section"></section>;
+  return (
+    <section className="section">
+      <div className="container relative h-full">
+        <div className="flex h-full flex-col items-center justify-center gap-x-24 text-center lg:flex-row lg:pt-16 lg:text-left">
+          <div className="order-2 max-h-96 flex-1 overflow-hidden lg:order-none lg:max-h-max">
+            <img src={womanImage} alt="Woman Image" />
+          </div>
+          <div className="z-10 flex flex-1 flex-col items-center justify-center pb-14 pt-32 lg:w-auto lg:items-start lg:pt-0">
+            <h1 className="h1">about me</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+              <b>Dolore recusandae</b> aliquid vel ea vitae dolor eius commodi
+              ipsum dolorum.
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+              quibusdam rerum et natus?
+            </p>
+            <Link to={"/portfolio"} className="btn">
+              View my work
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
