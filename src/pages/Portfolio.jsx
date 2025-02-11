57780930d0f41fc3c5b0c1cd04657eef768bfb02
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2025-02-11 14:04:03 +0600

    feat(portfolio): add Framer motion animations

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image1 from "../assets/img/portfolio/1.png";
 import Image2 from "../assets/img/portfolio/2.png";
 import Image3 from "../assets/img/portfolio/3.png";
 import Image4 from "../assets/img/portfolio/4.png";
 import { Link } from "react-router-dom";
+
+import { motion } from "motion/react";
+import { transitionOne } from "../transitions";
 
 const Portfolio = () => {
   return (
@@ -62,10 +65,22 @@ const Portfolio = () => {
           </div>
         </div>
       </section> */}
-      <section className="section">
+      <motion.section
+        initial={{ opacity: 0, y: "-100%" }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: "-100%" }}
+        transition={transitionOne}
+        className="section"
+      >
         <div className="container h-full">
           <div className="flex h-full flex-col items-center justify-center gap-y-12 lg:flex-row lg:gap-x-16 lg:pb-8">
-            <div className="text-center lg:text-start">
+            <motion.div
+              initial={{ opacity: 0, y: "-80%" }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: "-80%" }}
+              transition={transitionOne}
+              className="text-center lg:text-start"
+            >
               <h1 className="h1">portfolio</h1>
               <p className="mx-auto mb-12 max-w-[500px] px-6 md:max-w-[600px] lg:mx-0 lg:max-w-[500px] lg:px-0">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
@@ -79,7 +94,7 @@ const Portfolio = () => {
               <Link to={"/contact"} className="btn mx-auto lg:mx-0">
                 Hire me
               </Link>
-            </div>
+            </motion.div>
             <div className="grid grid-cols-2 lg:gap-2">
               <div className="h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[300px]">
                 <img
@@ -112,7 +127,7 @@ const Portfolio = () => {
             </div>
           </div>
         </div>
-      </section>
+      </motion.section>
     </>
   );
 };
